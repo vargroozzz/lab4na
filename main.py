0927c27f4aa81b99650361feae0a5d4991dfbd49
vargroozzz <edand2002@gmail.com>
2021-05-31 22:01:39 +0300

    spline interpolation ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from functools import reduce
-from itertools import product
-from math import sin, prod
+# from functools import reduce
+# from itertools import product
+from math import sin, prod, sqrt
 from typing import Final
 
 import matplotlib.pyplot as plt
@@ -55,6 +55,61 @@ def uppercase_n(points):
                            range(len(points))])
 
 
+def cubic_interp1d(points):
+    x, y = zip(*points)
+    x = np.asfarray(x)
+    y = np.asfarray(y)
+
+    size = len(x)
+
+    xdiff = np.diff(x)
+    ydiff = np.diff(y)
+
+    Li = np.empty(size)
+    Li_1 = np.empty(size - 1)
+    z = np.empty(size)
+
+    # [L][y] = [B]
+    Li[0] = sqrt(2 * xdiff[0])
+    Li_1[0] = 0.0
+    B0 = 0.0
+    z[0] = B0 / Li[0]
+
+    for i in range(1, size - 1, 1):
+        Li_1[i] = xdiff[i - 1] / Li[i - 1]
+        Li[i] = sqrt(2 * (xdiff[i - 1] + xdiff[i]) - Li_1[i - 1] * Li_1[i - 1])
+        Bi = 6 * (ydiff[i] / xdiff[i] - ydiff[i - 1] / xdiff[i - 1])
+        z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
+
+    i = size - 1
+    Li_1[i - 1] = xdiff[-1] / Li[i - 1]
+    Li[i] = sqrt(2 * xdiff[-1] - Li_1[i - 1] * Li_1[i - 1])
+    Bi = 0.0
+    z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
+
+    # [L.T][x] = [y]
+    z[i] = z[i] / Li[i]
+    for i in range(size - 2, -1, -1):
+        z[i] = (z[i] - Li_1[i - 1] * z[i + 1]) / Li[i]
+
+    def helper(x0):
+        index = x.searchsorted(x0)
+        np.clip(index, 1, size - 1, index)
+
+        xi1, xi0 = x[index], x[index - 1]
+        yi1, yi0 = y[index], y[index - 1]
+        zi1, zi0 = z[index], z[index - 1]
+        hi1 = xi1 - xi0
+
+        f0 = zi0 / (6 * hi1) * (xi1 - x0) ** 3 + \
+             zi1 / (6 * hi1) * (x0 - xi0) ** 3 + \
+             (yi1 / hi1 - zi1 * hi1 / 6) * (x0 - xi0) + \
+             (yi0 / hi1 - zi0 * hi1 / 6) * (xi1 - x0)
+        return f0
+
+    return helper
+
+
 a: Final = 0
 b: Final = 5
 N: Final = 5
@@ -62,13 +117,9 @@ xs = list(range(a, b + 1, int((b - a) / N)))
 ys = list(map(lambda x: fun(x), xs))
 pairs = list(zip(xs, ys))
 testvar = np.linspace(min(0, a - 1), b + 1, 100)
-# fig, ax = plt.subplots()
-# ax.plot(xs, ys, 'ro')
-# ax.plot(testvar, custom_interpolation(pairs)(testvar), label='custom')
-# ax.plot(testvar, uppercase_l(pairs)(testvar), label='Lagrange')
-# ax.plot(testvar, uppercase_n(pairs)(testvar), label='Newton')
-# ax.legend()
-fig, ((ax, custom), (lagrange, newton)) = plt.subplots(2, 2)
+
+fig, ((ax, custom), (lagrange, newton), (cubic_spline, _)) = plt.subplots(3, 2)
+
 ax.plot(testvar, custom_interpolation(pairs)(testvar), label='custom')
 ax.plot(testvar, uppercase_l(pairs)(testvar), label='Lagrange')
 ax.plot(testvar, uppercase_n(pairs)(testvar), label='Newton')
@@ -77,14 +128,17 @@ ax.plot(xs, ys, 'ro')
 custom.plot(xs, ys, 'ro')
 lagrange.plot(xs, ys, 'ro')
 newton.plot(xs, ys, 'ro')
+cubic_spline.plot(xs, ys, 'ro')
 
 custom.plot(testvar, custom_interpolation(pairs)(testvar), label='custom')
 lagrange.plot(testvar, uppercase_l(pairs)(testvar), label='Lagrange')
 newton.plot(testvar, uppercase_n(pairs)(testvar), label='Newton')
+cubic_spline.plot(testvar, cubic_interp1d(pairs)(testvar), label='Cubic spline')
 
 ax.legend()
 custom.legend()
 lagrange.legend()
 newton.legend()
+cubic_spline.legend()
 
 plt.show()
